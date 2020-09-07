540de67f78a7ede4de0d4408ad71c813ecb3a93a
Luca Barbato <lu_zero@gentoo.org>
2020-09-07 16:55:38 +0200

    Use the correct soname for Android

--- src/target.rs
+++ src/target.rs
@@ -74,7 +74,9 @@ impl Target {
         let os = &self.os;
         let env = &self.env;
 
-        if os == "linux" || os == "freebsd" || os == "dragonfly" || os == "netbsd" {
+        if os == "android" {
+            lines.push(format!("-Wl,-soname,lib{}.so", lib_name));
+        } else if os == "linux" || os == "freebsd" || os == "dragonfly" || os == "netbsd" {
             lines.push(format!("-Wl,-soname,lib{}.so.{}", lib_name, major));
         } else if os == "macos" {
             let line = format!("-Wl,-install_name,{1}/lib{0}.{2}.{3}.{4}.dylib,-current_version,{2}.{3}.{4},-compatibility_version,{2}",
