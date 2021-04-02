9de8d5afae18b27dc2f697c175d48ca5da6ddd56
Luca Barbato <lu_zero@gentoo.org>
2021-04-02 13:42:19 +0200

    Make the default profile for the install applet release
    It makes cargo-c behavior in line with cargo.

--- src/bin/cinstall.rs
+++ src/bin/cinstall.rs
@@ -3,7 +3,7 @@ use cargo::CliResult;
 use cargo::Config;
 
 use cargo_c::build::{cbuild, config_configure};
-use cargo_c::cli::subcommand_cli;
+use cargo_c::cli::subcommand_install;
 use cargo_c::install::cinstall;
 
 use structopt::clap::*;
@@ -11,7 +11,7 @@ use structopt::clap::*;
 fn main() -> CliResult {
     let mut config = Config::default()?;
 
-    let subcommand = subcommand_cli("cinstall", "Install the crate C-API");
+    let subcommand = subcommand_install("cinstall", "Install the crate C-API");
     let mut app = app_from_crate!()
         .settings(&[
             AppSettings::UnifiedHelpMessage,
@@ -41,7 +41,7 @@ fn main() -> CliResult {
     let mut ws = subcommand_args.workspace(&config)?;
 
     let (build_targets, install_paths, capi_config, _, _) =
-        cbuild(&mut ws, &config, &subcommand_args)?;
+        cbuild(&mut ws, &config, &subcommand_args, "release")?;
 
     cinstall(&ws, &capi_config, build_targets, install_paths)?;
 
