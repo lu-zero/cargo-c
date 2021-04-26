07220fc1bae2be0b33a68e8d053b887654afeaef
Luca Barbato <lu_zero@gentoo.org>
2021-04-26 15:07:33 +0200

    even more puzzling

--- grcov.sh
+++ grcov.sh
@@ -3,6 +3,11 @@
 # the github actions do not expand globs and somehow `grcov .` does not work as intended
 
 ls -l
+echo "Trying the glob"
 ls -l *.profraw
+echo "Trying the find"
 find . -name \*.profraw
+echo "Trying grcov"
+grcov *.profraw --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
+echo "Trying grcov ."
 grcov *.profraw --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
