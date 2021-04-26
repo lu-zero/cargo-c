73a24eebd899002895c965a3944eb85182d841f6
Luca Barbato <lu_zero@gentoo.org>
2021-04-26 15:21:31 +0200

    Switch ubuntu

--- grcov.sh
+++ grcov.sh
@@ -2,12 +2,12 @@
 #
 # the github actions do not expand globs and somehow `grcov .` does not work as intended
 
-ls -l
-echo "Trying the glob"
-ls -l *.profraw
-echo "Trying the find"
-find . -name \*.profraw
+MY_GLOB=`ls -l && ls -l *.profraw`
+echo "Trying the glob ${MY_GLOB}"
+
+echo "Trying the find `find . -name \*.profraw`"
+
 echo "Trying grcov"
 grcov *.profraw --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
 echo "Trying grcov ."
-grcov *.profraw --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
+grcov . --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
