df6b7057f0d2bc1f586f19c14364ed198ceda073
Luca Barbato <lu_zero@gentoo.org>
2021-04-26 14:23:47 +0200

    cleanups

--- grcov.sh
+++ grcov.sh
@@ -1,3 +1,6 @@
+# This is an ugly workaround.
+#
+# the github actions do not expand globs and somehow `grcov .` does not work as intended
 
-ls -l *.profraw
+ls -l
 grcov *.profraw --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
