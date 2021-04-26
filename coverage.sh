2b5f21e76913bf9c7ac0971c94efe80bda23dabc
Luca Barbato <lu_zero@gentoo.org>
2021-04-26 16:34:23 +0200

    Move the code coverage in a stand-alone script
    It should make simpler to run it locally.

---
+++ coverage.sh
@@ -0,0 +1,13 @@
+export LLVM_PROFILE_FILE="cargo-c-%p-%m.profraw"
+export RUSTFLAGS=-Zinstrument-coverage
+export CARGO_INCREMENTAL=0
+
+cargo +nightly build
+cargo +nightly test
+unset RUSTFLAGS
+
+target/debug/cargo-capi capi test --manifest-path=example-project/Cargo.toml
+target/debug/cargo-capi capi build --manifest-path=example-project/Cargo.toml
+target/debug/cargo-cinstall cinstall --manifest-path=example-project/Cargo.toml --destdir=/tmp/staging
+
+grcov . --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
