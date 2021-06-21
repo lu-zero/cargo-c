6a007f6b4a2d1fd800d0b36693838c48bad6bd25
Luca Barbato <lu_zero@gentoo.org>
2021-06-21 15:50:44 +0200

    Widen the code coverage

--- coverage.sh
+++ coverage.sh
@@ -7,8 +7,19 @@ cargo build
 cargo test
 unset RUSTFLAGS
 
+target/debug/cargo-capi capi --help
 target/debug/cargo-capi capi test --manifest-path=example-project/Cargo.toml
+target/debug/cargo-capi capi clean --manifest-path=example-project/Cargo.toml
 target/debug/cargo-capi capi build --manifest-path=example-project/Cargo.toml
+
+target/debug/cargo-cbuild --help
+target/debug/cargo-cbuild clean --manifest-path=example-project/Cargo.toml
+target/debug/cargo-cbuild cbuild --manifest-path=example-project/Cargo.toml
+target/debug/cargo-ctest metadata --help
+target/debug/cargo-ctest ctest --manifest-path=example-project/Cargo.toml
+
+target/debug/cargo-cinstall --help
 target/debug/cargo-cinstall cinstall --manifest-path=example-project/Cargo.toml --destdir=/tmp/staging
+target/debug/cargo-cinstall cinstall clean --manifest-path=example-project/Cargo.toml
 
 grcov . --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
