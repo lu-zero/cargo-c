16ecb60f7db937ca096caf644a0365a7ef003647
Luca Barbato <lu_zero@gentoo.org>
2021-06-30 09:24:02 +0200

    Support custom pre-generated and generated include
    The default patterns are:
    - pre-generated in: `{root_dir}/assets/capi/include/**/*`
    - generated in: `{out_dir}/capi/include/**/*`
    
    See #184

--- example-project/build.rs
+++ example-project/build.rs
@@ -1,4 +1,5 @@
 use cargo_metadata::*;
+use std::path::*;
 
 fn main() {
     let path = std::env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -9,4 +10,19 @@ fn main() {
         .unwrap();
 
     println!("{:?}", meta);
+
+    let out = std::env::var("OUT_DIR").unwrap();
+    let out = Path::new(&out);
+
+    let path = out.join("capi/include/");
+    let subdir = path.join("subdir");
+    let include = out.join("include");
+
+    std::fs::create_dir_all(&path).unwrap();
+    std::fs::create_dir_all(&subdir).unwrap();
+    std::fs::create_dir_all(&include).unwrap();
+
+    std::fs::write(path.join("generated.h"), "// Generated").unwrap();
+    std::fs::write(subdir.join("in_subdir.h"), "// Generated").unwrap();
+    std::fs::write(include.join("other_file.h"), "// Generated").unwrap();
 }
