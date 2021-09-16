266128fca77005f2f90ae63f1821b4e2470448b4
Luca Barbato <lu_zero@gentoo.org>
2021-09-16 14:19:34 +0200

    extend the code coverage

--- coverage.sh
+++ coverage.sh
@@ -8,21 +8,23 @@ cargo build
 cargo test
 unset RUSTFLAGS
 
-target/debug/cargo-capi capi --help
-target/debug/cargo-capi capi test --manifest-path=example-project/Cargo.toml
-target/debug/cargo-capi capi clean --manifest-path=example-project/Cargo.toml
-target/debug/cargo-capi capi build --manifest-path=example-project/Cargo.toml
+for project in example-project example-workspace; do
+    target/debug/cargo-capi capi --help
+    target/debug/cargo-capi capi test --manifest-path=${project}/Cargo.toml
+    target/debug/cargo-capi capi clean --manifest-path=${project}/Cargo.toml
+    target/debug/cargo-capi capi build --manifest-path=${project}/Cargo.toml
 
-target/debug/cargo-cbuild --help
-target/debug/cargo-cbuild clean --manifest-path=example-project/Cargo.toml
-target/debug/cargo-cbuild cbuild --manifest-path=example-project/Cargo.toml
-target/debug/cargo-ctest metadata --help
-target/debug/cargo-ctest ctest --manifest-path=example-project/Cargo.toml
+    target/debug/cargo-cbuild --help
+    target/debug/cargo-cbuild clean --manifest-path=${project}/Cargo.toml
+    target/debug/cargo-cbuild cbuild --manifest-path=${project}/Cargo.toml
+    target/debug/cargo-ctest metadata --help
+    target/debug/cargo-ctest ctest --manifest-path=${project}/Cargo.toml
 
-target/debug/cargo-cinstall --help
-target/debug/cargo-cinstall cinstall --manifest-path=example-project/Cargo.toml --destdir=/tmp/staging
-target/debug/cargo-cinstall cinstall clean --manifest-path=example-project/Cargo.toml
+    target/debug/cargo-cinstall --help
+    target/debug/cargo-cinstall cinstall --manifest-path=${project}/Cargo.toml --destdir=/tmp/staging
+    target/debug/cargo-cinstall cinstall clean --manifest-path=${project}/Cargo.toml
 
-target/debug/cargo-cinstall cinstall --manifest-path=example-project/Cargo.toml --destdir=/tmp/staging-win --target=x86_64-pc-windows-gnu
+    target/debug/cargo-cinstall cinstall --manifest-path=${project}/Cargo.toml --destdir=/tmp/staging-win --target=x86_64-pc-windows-gnu
+done
 
 grcov . --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
