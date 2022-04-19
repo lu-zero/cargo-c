e0e0683615ce06043ec436a55222219cd42244b7
Luca Barbato <lu_zero@gentoo.org>
2022-04-19 18:41:26 +0200

    Fix testing cross compilation

--- coverage.sh
+++ coverage.sh
@@ -2,9 +2,10 @@ export LLVM_PROFILE_FILE="cargo-c-%p-%m.profraw"
 export RUSTFLAGS=-Cinstrument-coverage
 export CARGO_INCREMENTAL=0
 
-rustup target add x86_64-pc-windows-gnu
+rustup default stable
 cargo build
 cargo test
+rustup target add x86_64-pc-windows-gnu
 unset RUSTFLAGS
 
 for project in example-project example-workspace; do
@@ -23,7 +24,7 @@ for project in example-project example-workspace; do
     target/debug/cargo-cinstall cinstall --manifest-path=${project}/Cargo.toml --destdir=/tmp/staging
     target/debug/cargo-cinstall cinstall clean --manifest-path=${project}/Cargo.toml
 
-    target/debug/cargo-cinstall cinstall --manifest-path=${project}/Cargo.toml --destdir=/tmp/staging-win --target=x86_64-pc-windows-gnu
+    target/debug/cargo-cinstall cinstall --manifest-path=${project}/Cargo.toml --destdir=/tmp/staging-win --target=x86_64-pc-windows-gnu --dlltool=x86_64-w64-mingw32-dlltool
 done
 
 grcov . --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../**' --ignore '/*' -o coverage.lcov
