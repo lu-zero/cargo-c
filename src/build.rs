lu-zero/cargo-c

commit 4c500de6b567a1461ffdecdfa746b8901194a68c

Update to cargo-0.67 and clap-4

src/build.rs
@@ -893,7 +893,7 @@ fn compile_with_exec<'a>(
         leaf_args.push("--print".into());
         leaf_args.push("native-static-libs".into());
 
-        if args.is_present("crt-static") {
+        if args.flag("crt-static") {
             leaf_args.push("-C".into());
             leaf_args.push("target-feature=+crt-static".into());
         }
@@ -1007,12 +1007,9 @@ pub fn cbuild(
         _ => vec!["staticlib", "cdylib"],
     };
 
-    let libkinds = if args.is_valid_arg("library-type") {
-        args.values_of("library-type")
-            .map_or_else(default_kind, |v| v.collect::<Vec<_>>())
-    } else {
-        default_kind()
-    };
+    let libkinds = args
+        .get_many::<String>("library-type")
+        .map_or_else(default_kind, |v| v.map(String::as_str).collect::<Vec<_>>());
     let only_staticlib = !libkinds.contains(&"cdylib");
     let only_cdylib = !libkinds.contains(&"staticlib");
 
@@ -1129,8 +1126,11 @@ pub fn cbuild(
                     .unwrap_or_else(|| PathBuf::from("dlltool"));
 
                 // dlltool argument overwrites environment var
-                if args.value_of_os("dlltool").is_some() {
-                    dlltool = args.value_of_os("dlltool").map(PathBuf::from).unwrap();
+                if args.contains_id("dlltool") {
+                    dlltool = args
+                        .get_one::<String>("dlltool")
+                        .map(PathBuf::from)
+                        .unwrap();
                 }
 
                 build_implib_file(ws, lib_name, &rustc_target, &root_output, &dlltool)?;
@@ -1207,14 +1207,14 @@ pub fn ctest(
     compile_opts.target_rustc_args = None;
 
     let ops = ops::TestOptions {
-        no_run: args.is_present("no-run"),
-        no_fail_fast: args.is_present("no-fail-fast"),
+        no_run: args.flag("no-run"),
+        no_fail_fast: args.flag("no-fail-fast"),
         compile_opts,
     };
 
-    let test_args = args.value_of("TESTNAME").into_iter();
-    let test_args = test_args.chain(args.values_of("args").unwrap_or_default());
-    let test_args = test_args.collect::<Vec<_>>();
+    let test_args = args.get_one::<String>("TESTNAME").into_iter();
+    let test_args = test_args.chain(args.get_many::<String>("args").unwrap_or_default());
+    let test_args = test_args.map(String::as_str).collect::<Vec<_>>();
 
     use std::ffi::OsString;
 

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "cargo-c"
-version = "0.9.14+cargo-0.66"
+version = "0.9.14+cargo-0.67"
 authors = ["Luca Barbato <lu_zero@gentoo.org>"]
 description = "Helper program to build and install c-like libraries"
 license = "MIT"
@@ -9,7 +9,7 @@ readme = "README.md"
 repository = "http://github.com/lu-zero/cargo-c"
 categories = ["command-line-utilities", "development-tools::cargo-plugins"]
 keywords = ["cargo", "cdylib"]
-rust-version = "1.65"
+rust-version = "1.66"
 
 [[bin]]
 name = "cargo-capi"
@@ -28,11 +28,11 @@ name = "cargo-ctest"
 path = "src/bin/ctest.rs"
 
 [dependencies]
-cargo = "0.66.0"
+cargo = "0.67.0"
 cargo-util = "0.2"
 semver = "1.0.3"
 log = "0.4"
-clap = { version="3.1", features=["color", "derive", "cargo"] }
+clap = { version="4.0.29", features=["color", "derive", "cargo"] }
 regex = "1.5.6"
 cbindgen = "0.24.3"
 toml = "0.5"

src/bin/capi.rs
@@ -18,7 +18,6 @@ fn main() -> CliResult {
     let cli_test = subcommand_test("test");
 
     let mut app = clap::command!()
-        .setting(AppSettings::DeriveDisplayOrder)
         .dont_collapse_args_in_usage(true)
         .allow_external_subcommands(true)
         .subcommand(
@@ -56,7 +55,7 @@ fn main() -> CliResult {
         }
     };
 
-    if subcommand_args.is_present("version") {
+    if subcommand_args.flag("version") {
         println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         return Ok(());
     }

src/bin/cbuild.rs
@@ -7,14 +7,11 @@ use cargo_c::cli::run_cargo_fallback;
 use cargo_c::cli::subcommand_build;
 use cargo_c::config::*;
 
-use clap::*;
-
 fn main() -> CliResult {
     let mut config = Config::default()?;
 
     let subcommand = subcommand_build("cbuild", "Build the crate C-API");
     let mut app = clap::command!()
-        .setting(AppSettings::DeriveDisplayOrder)
         .dont_collapse_args_in_usage(true)
         .allow_external_subcommands(true)
         .subcommand(subcommand);
@@ -33,7 +30,7 @@ fn main() -> CliResult {
         }
     };
 
-    if subcommand_args.is_present("version") {
+    if subcommand_args.flag("version") {
         println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         return Ok(());
     }

src/bin/cinstall.rs
@@ -8,14 +8,11 @@ use cargo_c::cli::subcommand_install;
 use cargo_c::config::config_configure;
 use cargo_c::install::cinstall;
 
-use clap::*;
-
 fn main() -> CliResult {
     let mut config = Config::default()?;
 
     let subcommand = subcommand_install("cinstall", "Install the crate C-API");
     let mut app = clap::command!()
-        .setting(AppSettings::DeriveDisplayOrder)
         .dont_collapse_args_in_usage(true)
         .allow_external_subcommands(true)
         .subcommand(subcommand);
@@ -34,7 +31,7 @@ fn main() -> CliResult {
         }
     };
 
-    if subcommand_args.is_present("version") {
+    if subcommand_args.flag("version") {
         println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         return Ok(());
     }

src/bin/ctest.rs
@@ -13,7 +13,6 @@ fn main() -> CliResult {
     let subcommand = subcommand_test("ctest");
 
     let mut app = clap::command!()
-        .setting(AppSettings::DeriveDisplayOrder)
         .dont_collapse_args_in_usage(true)
         .allow_external_subcommands(true)
         .subcommand(subcommand);
@@ -32,7 +31,7 @@ fn main() -> CliResult {
         }
     };
 
-    if subcommand_args.is_present("version") {
+    if subcommand_args.flag("version") {
         println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         return Ok(());
     }

src/cli.rs
@@ -1,44 +1,45 @@
+use std::ffi::{OsStr, OsString};
 use std::path::PathBuf;
 
-use cargo::util::command_prelude::AppExt;
-use cargo::util::command_prelude::{multi_opt, opt};
+use cargo::util::command_prelude::CommandExt;
+use cargo::util::command_prelude::{flag, multi_opt, opt};
 use cargo::util::{CliError, CliResult};
 
 use cargo_util::{ProcessBuilder, ProcessError};
 
-use clap::{Arg, ArgMatches, Command, CommandFactory, Parser};
+use clap::{Arg, ArgAction, ArgMatches, Command, CommandFactory, Parser};
 
 // TODO: convert to a function using cargo opt()
 #[allow(dead_code)]
 #[derive(Clone, Debug, Parser)]
 struct Common {
     /// Path to directory where target should be copied to
-    #[clap(long = "destdir", parse(from_os_str))]
+    #[clap(long = "destdir")]
     destdir: Option<PathBuf>,
     /// Directory path used to construct default values of
     /// includedir, libdir, bindir, pkgconfigdir
-    #[clap(long = "prefix", parse(from_os_str))]
+    #[clap(long = "prefix")]
     prefix: Option<PathBuf>,
     /// Path to directory for installing generated library files
-    #[clap(long = "libdir", parse(from_os_str))]
+    #[clap(long = "libdir")]
     libdir: Option<PathBuf>,
     /// Path to directory for installing generated headers files
-    #[clap(long = "includedir", parse(from_os_str))]
+    #[clap(long = "includedir")]
     includedir: Option<PathBuf>,
     /// Path to directory for installing generated executable files
-    #[clap(long = "bindir", parse(from_os_str))]
+    #[clap(long = "bindir")]
     bindir: Option<PathBuf>,
     /// Path to directory for installing generated pkg-config .pc files
-    #[clap(long = "pkgconfigdir", parse(from_os_str))]
+    #[clap(long = "pkgconfigdir")]
     pkgconfigdir: Option<PathBuf>,
     /// Path to directory for installing read-only data (defaults to {prefix}/share)
-    #[clap(long = "datarootdir", parse(from_os_str))]
+    #[clap(long = "datarootdir")]
     datarootdir: Option<PathBuf>,
     /// Path to directory for installing read-only application-specific data
     /// (defaults to {datarootdir})
-    #[clap(long = "datadir", parse(from_os_str))]
+    #[clap(long = "datadir")]
     datadir: Option<PathBuf>,
-    #[clap(long = "dlltool", parse(from_os_str))]
+    #[clap(long = "dlltool")]
     /// Use the provided dlltool when building for the windows-gnu targets.
     dlltool: Option<PathBuf>,
     #[clap(long = "crt-static")]
@@ -46,39 +47,37 @@ struct Common {
     crt_static: bool,
 }
 
-fn base_cli() -> Command<'static> {
+fn base_cli() -> Command {
     Common::command()
         .allow_external_subcommands(true)
-        .arg(opt("version", "Print version info and exit").short('V'))
+        .arg(flag("version", "Print version info and exit").short('V'))
+        .arg(flag("list", "List installed commands"))
+        .arg(opt("explain", "Run `rustc --explain CODE`").value_name("CODE"))
         .arg(
             opt(
                 "verbose",
                 "Use verbose output (-vv very verbose/build.rs output)",
             )
             .short('v')
-            .multiple_occurrences(true)
+            .action(ArgAction::Count)
             .global(true),
         )
-        .arg(opt("quiet", "No output printed to stdout").short('q'))
+        .arg_quiet()
         .arg(
             opt("color", "Coloring: auto, always, never")
                 .value_name("WHEN")
                 .global(true),
         )
-        .arg(opt("frozen", "Require Cargo.lock and cache are up to date").global(true))
-        .arg(opt("locked", "Require Cargo.lock is up to date").global(true))
-        .arg(opt("offline", "Run without accessing the network").global(true))
-        .arg(
-            multi_opt("config", "KEY=VALUE", "Override a configuration value")
-                .global(true)
-                .hide(true),
-        )
+        .arg(flag("frozen", "Require Cargo.lock and cache are up to date").global(true))
+        .arg(flag("locked", "Require Cargo.lock is up to date").global(true))
+        .arg(flag("offline", "Run without accessing the network").global(true))
+        .arg(multi_opt("config", "KEY=VALUE", "Override a configuration value").global(true))
         .arg(
             Arg::new("unstable-features")
                 .help("Unstable (nightly-only) flags to Cargo, see 'cargo -Z help' for details")
                 .short('Z')
                 .value_name("FLAG")
-                .multiple_occurrences(true)
+                .action(ArgAction::Append)
                 .global(true),
         )
         .arg_jobs()
@@ -103,7 +102,7 @@ fn base_cli() -> Command<'static> {
         .arg_build_plan()
 }
 
-pub fn subcommand_build(name: &str, about: &'static str) -> Command<'static> {
+pub fn subcommand_build(name: &'static str, about: &'static str) -> Command {
     base_cli()
         .name(name)
         .about(about)
@@ -115,7 +114,7 @@ pub fn subcommand_build(name: &str, about: &'static str) -> Command<'static> {
             )
             .global(true)
             .ignore_case(true)
-            .possible_values(["cdylib", "staticlib"]),
+            .value_parser(["cdylib", "staticlib"]),
         )
         .arg_release("Build artifacts in release mode, with optimizations")
         .arg_package_spec_no_all(
@@ -132,7 +131,7 @@ the --release flag will use the `release` profile instead.
         )
 }
 
-pub fn subcommand_install(name: &str, about: &'static str) -> Command<'static> {
+pub fn subcommand_install(name: &'static str, about: &'static str) -> Command {
     base_cli()
         .name(name)
         .about(about)
@@ -144,7 +143,7 @@ pub fn subcommand_install(name: &str, about: &'static str) -> Command<'static> {
             )
             .global(true)
             .ignore_case(true)
-            .possible_values(["cdylib", "staticlib"]),
+            .value_parser(["cdylib", "staticlib"]),
         )
         .arg(opt("debug", "Build in debug mode instead of release mode"))
         .arg_release(
@@ -164,7 +163,7 @@ the --debug flag will use the `dev` profile instead.
         )
 }
 
-pub fn subcommand_test(name: &str) -> Command<'static> {
+pub fn subcommand_test(name: &'static str) -> Command {
     base_cli()
         .trailing_var_arg(true)
         .name(name)
@@ -172,7 +171,7 @@ pub fn subcommand_test(name: &str) -> Command<'static> {
         .arg(
             Arg::new("args")
                 .help("Arguments for the test binary")
-                .multiple_values(true)
+                .num_args(0..)
                 .last(true),
         )
         .arg_release("Build artifacts in release mode, with optimizations")
@@ -187,9 +186,14 @@ pub fn subcommand_test(name: &str) -> Command<'static> {
 
 pub fn run_cargo_fallback(subcommand: &str, subcommand_args: &ArgMatches) -> CliResult {
     let cargo = std::env::var("CARGO_C_CARGO").unwrap_or_else(|_| "cargo".to_owned());
-    let mut args = vec![subcommand];
+    let mut args = vec![OsStr::new(subcommand)];
 
-    args.extend(subcommand_args.values_of("").unwrap_or_default());
+    args.extend(
+        subcommand_args
+            .get_many::<OsString>("")
+            .unwrap_or_default()
+            .map(OsStr::new),
+    );
     let err = match ProcessBuilder::new(cargo).args(&args).exec_replace() {
         Ok(()) => return Ok(()),
         Err(e) => e,

src/config.rs
@@ -15,17 +15,17 @@ fn setup_env() {
 pub fn config_configure(config: &mut Config, args: &ArgMatches) -> CliResult {
     let arg_target_dir = &args.value_of_path("target-dir", config);
     let config_args: Vec<_> = args
-        .values_of("config")
+        .get_many::<String>("config")
         .unwrap_or_default()
-        .map(String::from)
+        .map(|s| s.to_owned())
         .collect();
     config.configure(
-        args.occurrences_of("verbose") as u32,
-        args.is_present("quiet"),
-        args.value_of("color"),
-        args.is_present("frozen"),
-        args.is_present("locked"),
-        args.is_present("offline"),
+        args.verbose(),
+        args.flag("quiet"),
+        args.get_one::<String>("color").map(String::as_str),
+        args.flag("frozen"),
+        args.flag("locked"),
+        args.flag("offline"),
         arg_target_dir,
         &args
             .get_many::<String>("unstable-features")

commit e19c3d0f3e2a1103765a5e524a12fb8b03ef79b4

Fix `--dlltool` CLI argument
Regressed since commit 4c500de6b567a1461ffdecdfa746b8901194a68c.

src/build.rs
@@ -1129,7 +1129,7 @@ pub fn cbuild(
                 // dlltool argument overwrites environment var
                 if args.contains_id("dlltool") {
                     dlltool = args
-                        .get_one::<String>("dlltool")
+                        .get_one::<PathBuf>("dlltool")
                         .map(PathBuf::from)
                         .unwrap();
                 }