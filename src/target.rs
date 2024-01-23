73a4c751ddedf873f359ba258cc9f9bfa6b8e7e8
bitxeno <137328844+bitxeno@users.noreply.github.com>
2024-01-23 15:20:28 +0800

    Add tvOS support

--- src/target.rs
+++ src/target.rs
@@ -85,7 +85,7 @@ impl Target {
             } else {
                 format!("-Wl,-soname,lib{lib_name}.so")
             });
-        } else if os == "macos" || os == "ios" {
+        } else if os == "macos" || os == "ios" || os == "tvos" {
             let line = if capi_config.library.versioning {
                 format!("-Wl,-install_name,{1}/lib{0}.{5}.dylib,-current_version,{2}.{3}.{4},-compatibility_version,{5}",
                         lib_name, libdir.display(), major, minor, patch, sover)
